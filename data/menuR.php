f40a32592aade0b824ea948b58dc5e74306c1eae
IgrisUVG <igris@ushivangoga.ru>
2016-05-21 14:38:07 +0300

    160521

--- data/menuR.php
+++ data/menuR.php
@@ -7,6 +7,10 @@ $menu = array(
               );
 foreach ($menu as $item):
 ?>
-	<li><a href="<?=$item['link']?>" <?=$_SERVER['REQUEST_URI'] == $item['link'] ? 'class="verbergend"':''?>><?=$item['name']?></a></li>
+	<li>
+		<a href="<?=$item['link']?>" <?=$_SERVER['REQUEST_URI'] == $item['link'] ? 'class="verbergend"':''?>>
+			<?=$item['name']?>
+		</a>
+	</li>
 <?php endforeach;?>
 </ul><!--Конец правого меню-->
