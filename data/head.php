3a910b60e1c3b7b8159514611bbd0534caf1265b
IgrisUVG <igris@ushivangoga.ru>
2017-05-03 15:14:45 +0300

    Change head.php, add menuFull.min.js

--- data/head.php
+++ data/head.php
@@ -4,21 +4,22 @@
 <META NAME="KEYWORDS" CONTENT="Анна Шведова, художник, оформитель, дизайнер, картины, масло, графика, выставки, мозаика, оформление,
 дизайн, сумки, аксессуары, зарисовки.">
 <META NAME="DESCRIPTION" CONTENT="Творческая лаборатория Анны Шведовой. Картины, графика, и дизайн.">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>ТВОРЧЕСКАЯ ЛАБОРАТОРИЯ</title>
-<script type="text/javascript" src="/js/jquery.js"></script>
-<script type="text/javascript" src="/js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
 <!--script type="text/javascript" src="js/menu.js"></script-->
-<script type="text/javascript" src="/js/menuFull.js"></script>
-<script type="text/javascript" src="/js/ajax.js"></script>
+<script type="text/javascript" src="js/menuFull.js"></script>
+<script type="text/javascript" src="js/ajax.js"></script>
 <!--[if lte IE 8]>
-<script src="/js/respond.min.js"></script>
+<script src="js/respond.min.js"></script>
 <![endif]-->
-<link rel="icon" href="/images/as32.png" type="image/png">
-<link href="/css/style.css" rel="stylesheet" type="text/css">
-<!--link href="/css/stylesmall.css" rel="stylesheet" media="(max-width:480px)"-->
-<link type="text/css" href="/css/menu.css" rel="stylesheet">
-<!--link type="text/css" href="/css/form.css" rel="stylesheet"-->
-<link type='text/css' href='/css/contact.css' rel='stylesheet' media='screen' />
+<link rel="icon" href="images/as32.png" type="image/png">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/stylesmall.css" rel="stylesheet" media="(max-width:480px)">
+<link href="css/menu.css" rel="stylesheet" type="text/css">
+<link href="form.css" rel="stylesheet" type="text/css">
+<link type='text/css' href='css/contact.css' rel='stylesheet' media='screen' />
 <!--?php
 	include_once("analyticstracking.php")
 ?>
